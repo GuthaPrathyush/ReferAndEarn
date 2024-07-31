060bb54e4c2c9cbc8aa00d08458147734f5be181
Gutha Prathyush <prathyushgutha@gmail.com>
2024-08-01 00:35:50 +0530

    removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './components/Home';
 import Refer from './components/Refer';
